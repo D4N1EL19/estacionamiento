dd13a97f5aa3d4c5080c63386df19678e00a0008
sinai <al22760579@ite.edu.mx>
2023-05-11 17:45:43 -0700

    cambie la interfaz grafica

--- src/igu/InicioSesion.java
+++ src/igu/InicioSesion.java
@@ -90,7 +90,7 @@ public class InicioSesion extends javax.swing.JFrame {
         pnlPrincipal.setBackground(new java.awt.Color(255, 255, 255));
         pnlPrincipal.setPreferredSize(new java.awt.Dimension(500, 500));
 
-        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/persistencia/logo.png"))); // NOI18N
+        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/persistencia/346339429_9552708478103337_4631547997174124060_n.gif"))); // NOI18N
 
         lblUsuario.setFont(new java.awt.Font("Century Gothic", 0, 24)); // NOI18N
         lblUsuario.setText("Usuario");
@@ -104,6 +104,7 @@ public class InicioSesion extends javax.swing.JFrame {
             }
         });
 
+        btnEntrar.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         btnEntrar.setText("Iniciar sesion");
         btnEntrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -118,47 +119,47 @@ public class InicioSesion extends javax.swing.JFrame {
             .addGroup(pnlPrincipalLayout.createSequentialGroup()
                 .addGroup(pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(pnlPrincipalLayout.createSequentialGroup()
-                        .addGap(149, 149, 149)
-                        .addComponent(lblLogo))
-                    .addGroup(pnlPrincipalLayout.createSequentialGroup()
-                        .addGap(126, 126, 126)
-                        .addGroup(pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGap(604, 604, 604)
+                        .addGroup(pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jpdContrasenia, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(lblContrase)
-                            .addComponent(txtUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
-                            .addComponent(lblUsuario)
-                            .addComponent(jpdContrasenia)))
+                            .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblUsuario)))
                     .addGroup(pnlPrincipalLayout.createSequentialGroup()
-                        .addGap(199, 199, 199)
-                        .addComponent(btnEntrar)))
-                .addContainerGap(128, Short.MAX_VALUE))
+                        .addGap(662, 662, 662)
+                        .addComponent(btnEntrar))
+                    .addGroup(pnlPrincipalLayout.createSequentialGroup()
+                        .addGap(625, 625, 625)
+                        .addComponent(lblLogo)))
+                .addContainerGap(631, Short.MAX_VALUE))
         );
         pnlPrincipalLayout.setVerticalGroup(
             pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pnlPrincipalLayout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(165, 165, 165)
                 .addComponent(lblLogo)
-                .addGap(1, 1, 1)
+                .addGap(33, 33, 33)
                 .addComponent(lblUsuario)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(33, 33, 33)
                 .addComponent(lblContrase)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
                 .addComponent(jpdContrasenia, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(32, 32, 32)
+                .addGap(37, 37, 37)
                 .addComponent(btnEntrar)
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addContainerGap(275, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pnlPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(pnlPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 1481, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pnlPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(pnlPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 935, Short.MAX_VALUE)
         );
 
         pack();
@@ -169,29 +170,29 @@ public class InicioSesion extends javax.swing.JFrame {
     }//GEN-LAST:event_txtUsuarioActionPerformed
 
     private void btnEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEntrarActionPerformed
-        
+
         guardias.add(new Guardia("Daniel","Gutierrez","Ramirez",19,8,20004,"DaniSa19","190804","al22760562@ite.edu.mx",0));
         guardias.add(new Guardia("Saul","Martinez","Monge",19,2,20004,"saulikeapro","190204","al22760566@ite.edu.mx",1));
-        
+
         String usuario = txtUsuario.getText(), contra = null;
         char contrasenia[] = jpdContrasenia.getPassword();
         boolean guardia = false, administrativo = false;
-        
+
         contra = contra.valueOf(contrasenia);
-        
+
         guardia = encontrarGurdia(usuario, contra);
         administrativo = encontrarAdministrativo(usuario, contra);
-        
+
         if(guardia){
             //mandamos a llamar la interfaz de guardia
             System.out.println("Es guardia");
         }
-        
+
         if(administrativo){
             //mandamos a llamar la interfaz de administrativo
             System.out.println("Es administrativo");
         }
-        
+
         jpdContrasenia.setText("");
         txtUsuario.setText("");
     }//GEN-LAST:event_btnEntrarActionPerformed
