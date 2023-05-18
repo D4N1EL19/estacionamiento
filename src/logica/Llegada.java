54918010ac9e82b644c0ca31ea8913d0a78cae7f
Daniel <132523268+D4N1EL19@users.noreply.github.com>
2023-05-18 08:36:00 -0700

    Actualice la clase Llegada

--- src/logica/Llegada.java
+++ src/logica/Llegada.java
@@ -1,15 +1,55 @@
 package logica;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author daniel
  */
 
-public class Llegada {
+public final class Llegada {
     ////////////////////////////////////////////////////////////////////////////
     ////////////////////////// ATRIBUTOS DE LA CLASE ///////////////////////////
     ////////////////////////////////////////////////////////////////////////////
     
+    Calendar calendario = new GregorianCalendar();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
+    
     String fecha = "", hora = "";
     int numeroPersona = 0;
+    
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////// METODOS CONSTRUCTORES ///////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    
+    public Llegada(){
+        //vacio por defecto
+    }
+    
+    //para crear personas falsas
+    public Llegada(String fecha, String hora, int num){
+        this.fecha = fecha;
+        this.hora = fecha;
+        this.numeroPersona=num;
+    }
+    
+    //para agrefar oficialmente
+    public Llegada(int num){
+        this.fecha = obtenerFecha();
+        
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    //////////////////////////// METODOS PARTICULARES //////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    
+    //metodo para obtener la hora del sistema
+    public String obtenerFecha(){
+        calendario.setTime(new Date());
+        
+        return sdf.format(calendario.getTime());
+    }
 }
